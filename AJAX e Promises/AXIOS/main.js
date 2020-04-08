39270cf6065edef724c0550de47bd5fbd3e20eb4
Willian Ferreira dos santos <50432534+willianferreirax@users.noreply.github.com>
2020-04-07 21:57:32 -0300

    Add files via upload

---
+++ AJAX e Promises/AXIOS/main.js
@@ -0,0 +1,9 @@
+
+axios.get('https://api.github.com/users/diego3g')
+    .then(function(response){
+        console.log(response.data.avatar_url)
+    })
+
+    .catch(function(error){
+        console.warn(error)
+    })
